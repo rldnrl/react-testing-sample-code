4591bfdb65d8808473022f6baa3b9aad706808a3
rldnrl <devpark1002@gmail.com>
2022-01-09 13:26:51 +0900

    feat: set up emotion styled component test

--- jest.config.js
+++ jest.config.js
@@ -33,4 +33,7 @@ module.exports = {
     '^.+\\.module\\.(css|sass|scss)$',
   ],
   testEnvironment: 'jest-environment-jsdom',
+  snapshotSerializers: [
+    '@emotion/jest/serializer' /* if needed other snapshotSerializers should go here */
+  ]
 }
