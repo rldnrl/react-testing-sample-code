0076c9395a5c40b99adeb372791391489ae51e77
rldnrl <devpark1002@gmail.com>
2022-01-10 08:22:53 +0900

    feat: add comment for test

--- __tests__/home.spec.tsx
+++ __tests__/home.spec.tsx
@@ -2,5 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Home from "../pages";
 
 test("render home page", () => {
+  // 1) Rendering the component we want to test
   render(<Home />);
+
+  // 2) Finding the elements
+  const linkOfDocumentPage = screen.getByText(/Documentation/i);
+
+  // 3) Assertion
+  expect(linkOfDocumentPage).toBeInTheDocument();
 });
+
+test("should ", () => {});
