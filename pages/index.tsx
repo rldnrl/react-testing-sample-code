568ac8f70a4b1e1e5aeedf1f15dca4b7b4bf7654
rldnrl <devpark1002@gmail.com>
2022-01-12 09:27:05 +0900

    feat: implement on change logic with error handle logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,49 @@ import type { NextPage } from "next";
 import Input from "@components/Input";
 import Label from "@components/Label";
 import Head from "next/head";
-import Image from "next/image";
 import { ChangeEventHandler, useState } from "react";
 import emailValidate from "@utils/email-validate";
+import { Error } from "@components/Error";
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [isError, setIsError] = useState(false);
+  const [isMatchedPassword, setIsMatchedPassword] = useState(false);
+  const [isLessThan5Character, setIsLessThan5Character] = useState(false);
+  const [isEmail, setIsEmail] = useState(false);
+  const [isDisabledButton, setIsDisabledButton] = useState(false);
 
   const onChangeEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
-    if (!emailValidate(email)) {
-      setIsError(true);
-    }
+    setIsEmail(emailValidate(e.target.value));
     setEmail(e.target.value);
+  };
+
+  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setIsLessThan5Character(e.target.value.length < 5);
+    setIsMatchedPassword(e.target.value === confirmPassword);
+    setPassword(e.target.value);
+  };
+
+  const onChangeConfirmedPassword: ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setIsMatchedPassword(password === e.target.value);
+    setConfirmPassword(e.target.value);
+  };
+
+  const renderErrorMessage = () => {
+    if (!isEmail) {
+      return <Error>The email you input is invalid</Error>;
+    } else if (isLessThan5Character) {
+      return (
+        <Error>
+          The password you entered should contain 5 or more character
+        </Error>
+      );
+    } else if (!isMatchedPassword) {
+      return <Error>{`The password don't match. try again`}</Error>;
+    }
   };
 
   return (
@@ -45,7 +73,7 @@ const Home: NextPage = () => {
             id="password"
             name="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={onChangePassword}
           />
         </Label>
         <Label htmlFor="confirm-password">
@@ -55,11 +83,16 @@ const Home: NextPage = () => {
             id="confirm-password"
             name="confirm-password"
             value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={onChangeConfirmedPassword}
           />
         </Label>
-        {isError && <p>the email you input is invalid</p>}
-        <button type="submit">제출하기</button>
+        {renderErrorMessage()}
+        <button
+          type="submit"
+          disabled={!isEmail || isLessThan5Character || !isMatchedPassword}
+        >
+          제출하기
+        </button>
       </form>
     </div>
   );
