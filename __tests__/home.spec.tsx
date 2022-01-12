c1d1a94761c875e95c3136b8a175c3de6fb2827a
rldnrl <devpark1002@gmail.com>
2022-01-12 09:23:40 +0900

    feat: add test code. reduce duplication by using before each

--- __tests__/home.spec.tsx
+++ __tests__/home.spec.tsx
@@ -3,9 +3,11 @@ import UserEvent from "@testing-library/user-event";
 import Home from "../pages";
 
 describe("home page test", () => {
-  test("inputs should be initially empty ", () => {
+  beforeEach(() => {
     render(<Home />);
+  });
 
+  test("inputs should be initially empty ", () => {
     const emailInputElement = screen.getByRole<HTMLInputElement>("textbox");
     const passwordInputElement =
       screen.getByLabelText<HTMLInputElement>("Password");
@@ -18,52 +20,113 @@ describe("home page test", () => {
   });
 
   test("should be able to type an email", () => {
-    render(<Home />);
-
     const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
       name: /email/i,
     });
-
     UserEvent.type(emailInputElement, "python@gmail.com");
     expect(emailInputElement.value).toBe("python@gmail.com");
   });
 
   test("should be able to type an password", () => {
-    render(<Home />);
-
     const passwordInputElement =
       screen.getByLabelText<HTMLInputElement>("Password");
-
     UserEvent.type(passwordInputElement, "1234567");
     expect(passwordInputElement.value).toBe("1234567");
   });
 
   test("should be able to type an confirm password", () => {
-    render(<Home />);
-
     const confirmPasswordInputElement =
       screen.getByLabelText<HTMLInputElement>("Confirm Password");
-
     UserEvent.type(confirmPasswordInputElement, "1234567");
     expect(confirmPasswordInputElement.value).toBe("1234567");
   });
 
   test("should show email error message on invalid email", () => {
-    render(<Home />);
-
     const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
       name: /email/i,
     });
 
     UserEvent.type(emailInputElement, "pythongmail.com");
-
-    const submitButtonElement = screen.getByRole<HTMLButtonElement>("button", {
-      name: "제출하기",
-    });
-
     const emailErrorElement = screen.queryByText(
       /the email you input is invalid/i
     );
     expect(emailErrorElement).toBeInTheDocument();
+  });
+
+  test("should show password error message if password is less than 5 character", () => {
+    const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
+      name: /email/i,
+    });
+    UserEvent.type(emailInputElement, "python@gmail.com");
+
+    const passwordInputElement = screen.getByLabelText("Password");
+    UserEvent.type(passwordInputElement, "1234");
+    const passwordErrorElement = screen.queryByText(
+      /The password you entered should contain 5 or more character/i
+    );
+    expect(passwordErrorElement).toBeInTheDocument();
+  });
+
+  test("should show password error message if password is not matched origin password", () => {
+    const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
+      name: /email/i,
+    });
+    UserEvent.type(emailInputElement, "python@gmail.com");
+
+    const passwordInputElement = screen.getByLabelText("Password");
+    UserEvent.type(passwordInputElement, "12345");
+
+    const confirmedPasswordInputElement =
+      screen.getByLabelText("Confirm Password");
+    UserEvent.type(confirmedPasswordInputElement, "1234");
+    const confirmedPasswordErrorElement = screen.queryByText(
+      /The password don't match. try again/i
+    );
+    expect(confirmedPasswordErrorElement).toBeInTheDocument();
+  });
+
+  test("should show disabled button", () => {
+    const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
+      name: /email/i,
+    });
+    UserEvent.type(emailInputElement, "python@gmail.com");
+
+    const passwordInputElement = screen.getByLabelText("Password");
+    UserEvent.type(passwordInputElement, "12345");
+
+    const confirmedPasswordInputElement =
+      screen.getByLabelText("Confirm Password");
+    UserEvent.type(confirmedPasswordInputElement, "1234");
+
+    const button = screen.getByRole<HTMLButtonElement>("button", {
+      name: "제출하기",
+    });
+    expect(button).toBeDisabled();
+  });
+
+  test("should show no error message if every input is valid", () => {
+    const emailInputElement = screen.getByRole<HTMLInputElement>("textbox", {
+      name: /email/i,
+    });
+    const passwordInputElement = screen.getByLabelText("Password");
+    const confirmedPasswordInputElement =
+      screen.getByLabelText("Confirm Password");
+
+    UserEvent.type(emailInputElement, "python@gmail.com");
+    UserEvent.type(passwordInputElement, "12345");
+    UserEvent.type(confirmedPasswordInputElement, "12345");
+
+    const emailErrorElement = screen.queryByText(
+      /the email you input is invalid/i
+    );
+    const passwordErrorElement = screen.queryByText(
+      /The password you entered should contain 5 or more character/i
+    );
+    const confirmedPasswordErrorElement = screen.queryByText(
+      /The password don't match. try again/i
+    );
+    expect(emailErrorElement).not.toBeInTheDocument();
+    expect(passwordErrorElement).not.toBeInTheDocument();
+    expect(confirmedPasswordErrorElement).not.toBeInTheDocument();
   });
 });
