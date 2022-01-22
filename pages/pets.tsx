d76abc210451f0044eadc30d8370082db6cf183d
rldnrl <devpark1002@gmail.com>
2022-01-22 23:29:12 +0900

    feat: implement pets page

--- pages/pets.tsx
+++ pages/pets.tsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import type { NextPage } from "next";
+import Cards from "@components/Cards";
+import Filter from "@components/Filter";
+import { css, cx } from "@emotion/css";
+import isUndefined from "@utils/type-guard/isUndefined";
+import fetchCats, { Cat } from "@apis/fetchCats";
+import { useQuery } from "react-query";
 
-const Pets = () => {
+export type Gender = "any" | "male" | "female";
+
+export type Favorite = "any" | "favorite" | "not favorite";
+
+export type FilterType = {
+  gender: Gender;
+  favorite: Favorite;
+};
+
+type PetsProps = {
+  cats: Cat[];
+};
+
+const Pets: NextPage<PetsProps> = ({ cats }) => {
+  const { data } = useQuery("cats", fetchCats, { initialData: cats });
+  const [filteredCats, setFilteredCats] = useState<Cat[]>([...cats]);
+  const [filters, setFilters] = useState<FilterType>({
+    gender: "any",
+    favorite: "any",
+  });
+
+  useEffect(() => {
+    if (isUndefined(data)) return;
+
+    let catsFilters = [...data];
+
+    switch (filters.favorite) {
+      case "any":
+        setFilteredCats(catsFilters);
+        break;
+      case "favorite":
+        catsFilters = catsFilters.filter((cat) => cat.favored);
+        break;
+      case "not favorite":
+        catsFilters = catsFilters.filter((cat) => !cat.favored);
+        break;
+    }
+
+    switch (filters.gender) {
+      case "any":
+        setFilteredCats(catsFilters);
+        break;
+      case "male":
+        catsFilters = catsFilters.filter((cat) => cat.gender === "male");
+        break;
+      case "female":
+        catsFilters = catsFilters.filter((cat) => cat.gender === "female");
+        break;
+    }
+
+    setFilteredCats(catsFilters);
+  }, [data, filters]);
+
+  if (isUndefined(data)) return <p>Loading</p>;
+
   return (
-    <div>
-      Pets Page
+    <div className={cx("pets-container", petsContainerStyle)}>
+      <Filter filters={filters} onFiltersChange={setFilters} />
+      <Cards cats={filteredCats} onUpdateCats={setFilteredCats} />
     </div>
-  )
+  );
+};
+
+const petsContainerStyle = css`
+  display: flex;
+  justify-content: center;
+  padding: 3rem 0;
+`;
+
+export async function getStaticProps() {
+  const cats = await fetchCats();
+  return { props: { cats } };
 }
 
-export default Pets
+export default Pets;
